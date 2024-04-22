80aca890e93af7c1dbffde7806a599b6677fb5b2
RogelioAriasF <rogelioaf@hotmail.com>
2024-04-22 15:18:30 -0600

    Commit inicial

---
+++ ProyectoWeb_Martes/ProyectoWeb_Martes/Controllers/InicioController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoWeb_Martes.Controllers
+{
+    public class InicioController : Controller
+    {
+        // GET: Inicio
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public ActionResult RegistrarUsuario()
+        {
+            return View();
+        }
+
+        public ActionResult PantallaPrincipal()
+        {
+            return View();
+        }
+    }
+}
